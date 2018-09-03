76e9d10ac6e9ebdeed0c379d4bdcd048136d4493
clairemacdonald <claire.macdonald@blis.com>
2018-09-03 10:11:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function nameToAttributes(array) {
 
 function attributesToPhrase(drivers) {
   return array.map(e=>{
-    const name =nameAndHometown[0];
+    const name = nameAndHometown[0];
     const hometown =nameAndHometown[1];
     return name + "is from" + hometown;
   });
