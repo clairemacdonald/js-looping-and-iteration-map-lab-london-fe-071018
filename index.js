c1c95a9bfbda558d566d511ced176c8b541bb311
clairemacdonald <claire.macdonald@blis.com>
2018-09-03 09:22:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution in this file.
+
+function lowerCaseDrivers(drivers) {
+return drivers.map(driver => driver.toLowerCase());
+  
+}
+
+function nameToAttributes(array) {
+  return array.map(e=>{
+    const nameAndSurname = e.split(" ");
+    return {
+      firstName: nameAndSurname[0],
+      lastName: nameAndSurname[1]
+    };
+  });
+}
+
+
+function attributesToPhrase(drivers) {
+  
+ 
+}
